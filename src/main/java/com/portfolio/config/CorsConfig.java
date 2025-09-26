7977fa3f3b798166c749be94fd3e40eeefe520be
Saksham Singla <90709664+SakshamSingla1@users.noreply.github.com>
2025-09-26 09:59:59 +0530

    Added Dynamically CORS Handling

--- src/main/java/com/portfolio/config/CorsConfig.java
+++ src/main/java/com/portfolio/config/CorsConfig.java
@@ -1,28 +1,47 @@
 package com.portfolio.config;
 
+import com.portfolio.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
-    // Read allowed origins from env/application.properties (comma separated)
-    @Value("${app.cors.allowed-origins}")
-    private String[] allowedOrigins;
+    @Autowired
+    ProfileRepository profileRepository;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins) // pulled from env/properties
+                List<String> allowedOrigins = profileRepository.findAll()
+                                .stream()
+                                .map(profile -> profile.getWebsiteUrl())
+                                .filter(url -> url != null && !url.isBlank())
+                                .toList();
+                if(allowedOrigins.isEmpty()) {
+                    allowedOrigins = List.of(
+                            "http://localhost:3000",
+                            "http://127.0.0.1:3000",
+                            "http://localhost:5173",
+                            "http://127.0.0.1:5173",
+                            "http://localhost:5174",
+                            "http://127.0.0.1:5174");
+                }
+                registry.addMapping("/api/**")
+                        .allowedOrigins(allowedOrigins.toArray(new String[0])) // pulled from env/properties
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+                System.out.println(allowedOrigins);
             }
         };
     }
