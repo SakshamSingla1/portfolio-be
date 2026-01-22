1ee0d56d59d95031e076a13e71ad5783bccc7e3a
Saksham Singla <saksham.singla@abainfotech.com>
2026-01-22 18:09:11 +0530

    Added the project images

--- src/main/java/com/portfolio/dtos/ProjectRequest.java
+++ src/main/java/com/portfolio/dtos/ProjectRequest.java
@@ -1,12 +1,15 @@
 package com.portfolio.dtos;
 
+import com.portfolio.dtos.ProjectImages.ProjectImageRequest;
 import com.portfolio.enums.WorkStatusEnum;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@Builder
 public class ProjectRequest {
     private String profileId;
     private String projectName;
@@ -15,6 +18,6 @@ public class ProjectRequest {
     private LocalDate projectStartDate;
     private LocalDate projectEndDate;
     private WorkStatusEnum workStatus;
-    private String projectImageUrl;
     private List<String> skillIds;
+    private List<ProjectImageRequest> projectImages;
 }
--- src/main/java/com/portfolio/services/ProjectService.java
+++ src/main/java/com/portfolio/services/ProjectService.java
@@ -1,11 +1,14 @@
 package com.portfolio.services;
 
+import com.portfolio.dtos.ImageUploadResponse;
 import com.portfolio.dtos.ProjectRequest;
 import com.portfolio.dtos.ProjectResponse;
 import com.portfolio.exceptions.GenericException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ProjectService {
@@ -15,6 +18,8 @@ public interface ProjectService {
     ProjectResponse update(String id, ProjectRequest request) throws GenericException;
 
     ProjectResponse getById(String id) throws GenericException;
+
+    ImageUploadResponse uploadProjectImage(String profileId, MultipartFile file) throws IOException, GenericException;
 
     String delete(String id) throws GenericException;
 
