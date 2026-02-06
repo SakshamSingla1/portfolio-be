e2c1eb6b2421b3eb95a018988e76f83133421948
Saksham Singla <saksham.singla@abainfotech.com>
2026-02-06 11:48:28 +0530

    Added more enums

--- src/main/java/com/portfolio/enums/DegreeEnum.java
+++ src/main/java/com/portfolio/enums/DegreeEnum.java
@@ -3,11 +3,36 @@ package com.portfolio.enums;
 public enum DegreeEnum {
     HIGH_SCHOOL("10th"),
     SENIOR_SECONDARY("12th"),
-    BACHELORS("Bachelors"),
-    BTECH("Bachelors of Technology"),
-    MASTERS("Masters"),
+
     DIPLOMA("Diploma"),
+    ADVANCED_DIPLOMA("Advanced Diploma"),
+
+    ASSOCIATE("Associate Degree"),
+
+    BACHELORS("Bachelors"),
+    BTECH("Bachelor of Technology"),
+    BE("Bachelor of Engineering"),
+    BSC("Bachelor of Science"),
+    BA("Bachelor of Arts"),
+    BCOM("Bachelor of Commerce"),
+    BCA("Bachelor of Computer Applications"),
+    BBA("Bachelor of Business Administration"),
+
+    MASTERS("Masters"),
+    MTECH("Master of Technology"),
+    ME("Master of Engineering"),
+    MSC("Master of Science"),
+    MA("Master of Arts"),
+    MCOM("Master of Commerce"),
+    MBA("Master of Business Administration"),
+    MCA("Master of Computer Applications"),
+
     PHD("PhD"),
+    POST_DOCTORATE("Post Doctorate"),
+
+    CERTIFICATION("Certification"),
+    PROFESSIONAL_CERTIFICATE("Professional Certificate"),
+
     OTHER("Other");
 
     private final String displayName;
