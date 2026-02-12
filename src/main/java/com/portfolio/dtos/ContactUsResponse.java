e916c6ae4ae891c3728eed648ae270c647838e49
Saksham Singla <saksham.singla@abainfotech.com>
2026-02-12 17:37:56 +0530

    Added the status management for the contact us status api

--- src/main/java/com/portfolio/dtos/ContactUsResponse.java
+++ src/main/java/com/portfolio/dtos/ContactUsResponse.java
@@ -1,5 +1,6 @@
 package com.portfolio.dtos;
 
+import com.portfolio.enums.ContactUsStatusEnum;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,5 +15,6 @@ public class ContactUsResponse {
     private String email;
     private String message;
     private String phone;
+    private ContactUsStatusEnum status;
     private LocalDateTime createdAt;
 }
