0d518efc6844e5326a0ddef6858667abe05c5dcd
Saksham Singla <saksham.singla@abainfotech.com>
2026-02-19 10:20:41 +0530

    Logo Updates

--- src/main/java/com/portfolio/dtos/logo/LogoDropdown.java
+++ src/main/java/com/portfolio/dtos/logo/LogoDropdown.java
@@ -4,10 +4,14 @@ import com.portfolio.enums.SkillCategoryEnum;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class LogoDropdown {
     private String id;
     private String name;
     private String url;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
